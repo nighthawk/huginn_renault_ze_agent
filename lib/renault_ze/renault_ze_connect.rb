489cc832775c820aca8dd99f24043baaff40581f
Adrian Schoenig <adrian.schoenig@gmail.com>
2019-04-07 22:31:45 +0200

    Getting things to work

--- lib/renault_ze/renault_ze_connect.rb
+++ lib/renault_ze/renault_ze_connect.rb
@@ -7,9 +7,12 @@ module RenaultZE
 
     def login
       response = HTTParty.post(
-        "https://www.services.renault-ze.com/api/user/login", 
+        "https://www.services.renault-ze.com/api/user/login",
+        headers: {"Content-Type": "application/json"}, 
         body: {username: @username, password: @password}.to_json
       )
+      raise 'Unexpected response code: #{response}' unless response.code == 200
+      
       content = JSON.parse response.body
       return {
         token: content["token"],
@@ -18,13 +21,21 @@ module RenaultZE
     end
 
     def get_battery(creds)
+      raise 'Missing token in input: #{creds}' if creds[:token].nil?
+      raise 'Missing VIN in input: #{creds}' if creds[:vin].nil?
+
       response = HTTParty.get(
         "https://www.services.renault-ze.com/api/vehicle/#{creds[:vin]}/battery",
-        headers: {"Authorization": "Bearer #{creds["token"]}"}
+        headers: {"Authorization": "Bearer #{creds[:token]}"}
       )
+      raise 'Unexpected response code: #{response}' unless response.code == 200
 
       result = JSON.parse response.body
-      result["last_update"] = result["last_update"] / 1000
+      unless result["last_update"].nil?
+        epoch = result["last_update"] / 1000
+        result["last_update"] = epoch
+        result["last_update_hours_ago"] = (Time.now - Time.at(epoch)) / 3600
+      end
       return result
     end
   end
